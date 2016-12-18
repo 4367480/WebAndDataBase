2c35d71e884a85e17efd8bffef559c3bd6f625ac
4367480 <spamcheck7@hotmail.com>
2016-12-18 22:08:48 +0900

    no message

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var mysqlconnection = mysql.createConnection({
     host: 'localhost',
     user: 'webdata',
     password: 'webdata',
-    database: 'todos'
+    database: 'doesnotexist'
 });
 mysqlconnection.connect();
 
